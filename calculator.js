fcb2c2853a19f4f4b800df8e9a7f45ec6ea5e938
Tim A. Conner <timaconner1@gmail.com>
2017-11-27 09:37:38 -0600

    Added functions add, subtract, multiply, divide, and mathmaticalFunction.

---
+++ calculator.js
@@ -0,0 +1,48 @@
+/*
+  Create a function that multiplies two numbers
+  passed in as arguments. Return the product.
+ */
+
+ function multiply(x, y){
+     return (x * y);
+ }
+
+/*
+  Create a function that adds two numbers
+  passed in as arguments. Return the sum.
+ */
+function add(x, y){
+    return (x + y);
+}
+
+/*
+  Create a function that subtracts two numbers
+  passed in as arguments. Return the difference.
+ */
+function subtract(x, y){
+    return (x - y);
+}
+
+/*
+  Create a function that divides two numbers
+  passed in as arguments. Return the quotient.
+ */
+
+function divide(x, y){
+    return (x / y);
+}
+
+/*
+  Create a function that accepts three arguments.
+    1. First number
+    2. Second number
+    3. A function that performs an operation on them
+
+  Return the value of the operation.
+ */
+
+ function mathmaticalFunction(x, y, func){
+     
+ }
+
+ 
